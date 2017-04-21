5e2b1e116fe2ee5e671e914bf8a94d724bfdee26
andrea <andreafasci@gmail.com>
2017-04-21 15:13:12 +0100

    Added server_action.php

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Hello, Andrea!";
-echo "Hello Again, Mr. Andreaa! :)"
-?>
---
+++ server_action.php
@@ -0,0 +1,17 @@
+<?php
+echo "Welcome! :)";
+echo $_POST["name"];
+echo $_POST["email"];
+?>
+
+<html>
+<body>
+
+<form action="" method="post">
+    Name: <input title="" type="text" name="name"><br>
+    E-mail: <input title="" type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
